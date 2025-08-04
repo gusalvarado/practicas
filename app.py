d519234f073931ea135290f1b507cc1110c30b2b
gusalvarado <galvaradocrue@gmail.com>
2025-08-04 14:55:32 -0600

    remove auth

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import streamlit as st
 from core.chat import chat
 from tools.auth import Authenticator
-from core.admin import admin
-from core.signup import signup
+# from core.admin import admin
+# from core.signup import signup
 
 auth = Authenticator()
 auth.restore_session()
 
 st.session_state.setdefault("view", "Home")
 
-if not st.session_state.get("authenticated"):
-    page = st.radio("Choose page", ["Login", "Signup"], horizontal=True)
-    if page == "Login":
-        auth.login()
-    elif page == "Signup":
-        signup()
-    st.stop()
-auth.logout()
-user = st.session_state.user_info
-st.sidebar.success(f"{user['name']} ({user['role']})")
+# if not st.session_state.get("authenticated"):
+#     page = st.radio("Choose page", ["Login", "Signup"], horizontal=True)
+#     if page == "Login":
+#         auth.login()
+#     elif page == "Signup":
+#         signup()
+#     st.stop()
+# auth.logout()
+# user = st.session_state.user_info
+# st.sidebar.success(f"{user['name']} ({user['role']})")
 
 menu = ["Chat"]
 st.session_state.view = st.radio("Navigate", menu, horizontal=True)
